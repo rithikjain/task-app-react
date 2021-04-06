dd9b4a2b29ba8d083b0cc7b51f613ad5623bb332
Rithik Jain <rithik.jain3006@gmail.com>
2021-04-06 21:09:27 +0530

    feat: add css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello from react</h1>
+    <div className="container">
+      <Header />
     </div>
   );
 }
