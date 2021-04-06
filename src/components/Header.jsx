d1871608866e22f2a778cbb394979393a13731bf
Rithik Jain <rithik.jain3006@gmail.com>
2021-04-06 21:30:00 +0530

    feat: add button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
+import Button from './Button'
+
 const Header = (props) => {
+
+  const onClick = () => {
+    console.log('Click');
+  }
+
   return (
-    <header>
+    <header className='header'>
       <h1>{props.title}</h1>
+      <Button color='green' text='Add' onClick={onClick} />
     </header>
   )
 }
